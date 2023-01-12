b3179063ff32f465cf14d842938410a96b69c4d1
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-01-12 14:48:41 +0200

    Add: violating drones filtered from the capture snapshot according to positionX & positionY

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import XMLParser from "react-xml-parser";
+const convert = require("xml-js");
 
 function App() {
-  const [drones, setDrones] = useState([]);
+  const [drones, setDrones] = useState(0);
 
   useEffect(() => {
     getData();
@@ -13,14 +13,34 @@ function App() {
   const getData = async () => {
     try {
       var response = await axios.get("/drones", {});
-      var parsed = new XMLParser().parseFromString(response.data);
 
-      console.log(parsed.children[1].children);
+      const parsed = JSON.parse(
+        convert.xml2json(response.data, { compact: true, spaces: 2 })
+      );
+      let capturedDrones = parsed.report.capture.drone;
+      console.log(capturedDrones);
+
+      var violators = capturedDrones.filter((drone) => {
+        if (drone.positionX._text < 150000) {
+          return false;
+        }
+        if (drone.positionX._text > 350000) {
+          return false;
+        }
+        if (drone.positionY._text < 150000) {
+          return false;
+        }
+        if (drone.positionY._text > 350000) {
+          return false;
+        }
+        return true;
+      });
+      console.log(violators);
     } catch (error) {
       console.error(error);
     }
   };
-  return <div className="App"></div>;
+  return <div className="App">{drones}</div>;
 }
 
 export default App;
