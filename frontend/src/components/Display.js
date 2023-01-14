50258ff5f6388f9b2fb03030909c471ed2c093aa
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-01-14 17:33:43 +0200

    Add: simple css styling

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -1,21 +1,28 @@
+import "./display.css";
+
 const Display = ({ filteredDrones }) => {
   return (
     <div className="DisplayScreen">
       <div>
         {filteredDrones.map((drone) => (
-          <div>
-            <h3>
-              {drone.firstName} - {drone.lastName}
-            </h3>
-            <p>
-              email: {drone.email} phone: {drone.phoneNumber}
-            </p>
-            <p>
-              distance:
-              {Math.round((drone.closestDistance / 1000) * 100) / 100}
-              meters
-            </p>
-            <p>Last seen: {drone.createdDt.toLocaleString()}</p>
+          <div className="pilotCard">
+            <div className="data">
+              <h3 className="name">
+                {drone.firstName} {drone.lastName}
+              </h3>
+              <div className="contactInfo">
+                <p>email: {drone.email}</p>
+                <p>phone: {drone.phoneNumber}</p>
+              </div>
+              <div className="statistics">
+                <p>
+                  Closest confirmed distance:
+                  {Math.round((drone.closestDistance / 1000) * 100) / 100}
+                  meters
+                </p>
+                <p>Last Seen: {drone.createdDt.toLocaleString()}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
