0d4d544544bb6ef5896e7788e82d215d8275a56f
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2023-01-15 15:04:15 +0200

    Documentation: comment Utils.js

--- frontend/src/Utils/Utils.js
+++ frontend/src/Utils/Utils.js
@@ -1,3 +1,9 @@
+/*
+This function calculates distance between two points in 2 dimensional coordinate system.
+rone object here represents drone which has positions y and x, and centerpoint represents the nest.
+Centerpoint object has lat and lon properties.
+*/
+
 export const calculateDistance = (drone, centerPoint) => {
   let distance = Math.sqrt(
     Math.pow(drone.positionX._text - centerPoint.lon, 2) +
@@ -7,12 +13,24 @@ export const calculateDistance = (drone, centerPoint) => {
   return distance;
 };
 
+/*
+This function checks whether certain amount of minutes has passed since given date.
+Returns boolean value true if said amount of minutes has passed.
+*/
+
 export const minutesPassed = (date, minutes) => {
   let d = new Date(date);
   d.setMinutes(d.getMinutes() + minutes);
 
   return new Date() > d;
 };
+
+/*
+This function filters the violating drones from the received report of all drones.
+It has certain extremes which are first taken into account and then
+uses Pythagorean theorem to check whether the drone is inside the circle
+which radius is given as argument.
+*/
 
 export const filterViolators = (capturedDrones, centerPoint, radius) => {
   let violators = capturedDrones.filter((drone) => {
